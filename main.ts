acff12fe4984275fdcb2a20dc7919030b2cb2c34
1711087 <104405424+1711087@users.noreply.github.com>
2022-06-08 10:40:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,157 +1,33 @@
 basic.forever(function () {
-    music.playTone(262, music.beat(BeatFraction.Half))
-    basic.pause(200)
-    music.playTone(262, music.beat(BeatFraction.Half))
-})
-basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )))
-    basic.showNumber(sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-    ))
     if (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-    ) > 5) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # . # .
-            . . # . .
-            . . . . .
-            `)
+    ) < 1) {
         basic.showLeds(`
             . . # . .
             . # . # .
             # . . . #
-            . # . # .
-            . . # . .
-            `)
-        basic.showLeds(`
-            . # # # .
-            # # . # #
+            # # # # #
             # . . . #
-            # # . # #
-            . # # # .
             `)
-        basic.showLeds(`
-            . # # # #
-            # # . # #
-            # . . . #
-            # # . # #
-            . # # # .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . . . #
-            # # . # #
-            . # # # .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . . . #
-            # # . # #
-            . # # # .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . . . #
-            # # . # #
-            . # # # #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . . . #
-            # # . # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . # . #
-            # # . # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . # # #
-            # # . # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # . # # #
-            # # . # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # . # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            # # # # .
-            # # # # .
-            # # # # .
-            # # # # .
-            # # # # .
-            `)
+    }
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 1) {
         basic.showLeds(`
             # # # . .
+            # . . # .
             # # # . .
+            # . . # .
             # # # . .
-            # # # . .
-            # # # . .
-            `)
-        // 
-        basic.showLeds(`
-            # # . . .
-            # # . . .
-            # # . . .
-            # # . . .
-            # # . . .
-            `)
-        basic.showLeds(`
-            # . . . .
-            # . . . .
-            # . . . .
-            # . . . .
-            # . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
             `)
     }
 })
